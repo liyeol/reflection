25e48ad873e2686da01675d0562462a3847982b2
liyeol <98913681+liyeol@users.noreply.github.com>
2023-02-05 14:21:24 +0000

    1

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection;
+
+namespace AssemblyReflectionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the assembly path: ");
+            string assemblyPath = Console.ReadLine();
+
+            Assembly assembly = Assembly.LoadFrom(assemblyPath);
+
+            Type[] types = assembly.GetTypes();
+
+            foreach (Type type in types)
+            {
+                Console.WriteLine("Class: " + type.Name);
+
+                MethodInfo[] methods = type.GetMethods();
+
+                foreach (MethodInfo method in methods)
+                {
+                    Console.WriteLine("\tMethod: " + method.Name);
+                    Console.WriteLine("\t\tReturn Type: " + method.ReturnType.Name);
+                    Console.WriteLine("\t\tArguments:");
+
+                    ParameterInfo[] parameters = method.GetParameters();
+
+                    foreach (ParameterInfo parameter in parameters)
+                    {
+                        Console.WriteLine("\t\t\t" + parameter.ParameterType.Name + " " + parameter.Name);
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
